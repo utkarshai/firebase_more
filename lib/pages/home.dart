64297b1e4213441bc7a365211f00805a34b47e2e
Utkarsh Raj <rajutkarshai@gmail.com>
2019-08-16 15:11:31 +0530

    second commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,57 +1,37 @@
-
-    
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-class Home extends StatelessWidget {
-  const Home({Key key, this.user}) : super(key: key);
+class Home extends StatefulWidget {
+   const Home({
+Key key,
+@required this.user
+  }):super(key:key);
   final FirebaseUser user;
+  @override
+  _HomeState createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home ${user.email}'),
-      ),
+      appBar: AppBar(title: Text("${widget.user.email}"),),
       body: StreamBuilder<DocumentSnapshot>(
-        stream: Firestore.instance
-            .collection('users')
-            .document(user.uid)
-            .snapshots(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else if (snapshot.hasData) {
-            return checkRole(snapshot.data);
+        stream: Firestore.instance.collection('users').document(user.uid).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot){
+          if(snapshot.hasError){
+            return Text("error!! : ${snapshot.error}");
           }
-          return LinearProgressIndicator();
+          switch(snapshot.connectionState){
+            case ConnectionState.waiting: return Text("loading....");
+            default:
+            return Text("snapshot.data['name']");
+          }
         },
+        initialData: ,
       ),
     );
-  }
-
-  Center checkRole(DocumentSnapshot snapshot) {
-    if (snapshot.data == null) {
-      return Center(
-        child: Text('no data set in the userId document in firestore'),
-      );
-    }
-    if (snapshot.data['role'] == 'admin') {
-      return adminPage(snapshot);
-    } else {
-      return userPage(snapshot);
-    }
-  }
-
-  Center adminPage(DocumentSnapshot snapshot) {
-    return Center(
-        child: Text('${snapshot.data['role']} ${snapshot.data['name']}'));
-  }
-
-  Center userPage(DocumentSnapshot snapshot) {
-    return Center(child: Text(snapshot.data['name']));
+    
   }
 }
-
